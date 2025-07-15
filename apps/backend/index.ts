101186d346f2b22d158f4b5c8b7d787ec7911b58
Levent Celik <levent.lc97@gmail.com>
2025-07-15 18:39:29 +0200

    added endpoint for english alt arts

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -85,4 +85,47 @@ app.get("/api/images/en/hash", async () => {
   }
 });
 
+app.get("/api/images/en-alt/hash", async () => {
+  console.log("/api/images/en-alt/hash called");
+  const IDENTIFIER = "images-final";
+  const EN_ALT_KEY = "pixelbornaltart.zip";
+
+  try {
+    const response = await fetch(`https://archive.org/metadata/${IDENTIFIER}/`);
+    if (!response.ok) {
+      throw new Error(
+        `Status Code is not OK: ${response.status} ${response.statusText}`
+      );
+    }
+
+    const metadata = await response.json();
+    const fileMetadata = metadata.files.find(
+      (file: any) => file.name === EN_ALT_KEY
+    );
+    if (!fileMetadata) {
+      throw new Error("❌ File metadata not found");
+    }
+    return new Response(
+      JSON.stringify({
+        hash: fileMetadata.md5,
+        downloadUrl: `https://archive.org/download/${IDENTIFIER}/${EN_ALT_KEY}`,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.error("❌ Error:", error);
+    return new Response(
+      JSON.stringify({ error: "Failed to get image zip hash." }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+});
+
 export default app;
